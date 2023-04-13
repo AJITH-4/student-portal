abb3c3e67f7821e8b11aa40552e6dd0b64f64b22
Ajith-FSD <126141461+AJITH-4@users.noreply.github.com>
2023-04-13 12:02:38 +0530

    Add files via upload

---
+++ DOM.js
@@ -0,0 +1,22 @@
+// var element =document.createElement("div");
+// console.log(element);
+// element.innerHTML = "this is div";
+// document.body.append(element);
+
+
+var col = document.createElement("div");
+col.className = "col";
+col.innerHTML = "Hi this is Ajith";
+document.body.append(col);
+//Parent.append(child)
+// div.append(div);
+// var div=document.createElement("div");
+// div.className = "row";
+// //Parent.append(child)
+// div.append(div);
+// var div=document.createElement("div");
+// div.className = "container";
+// //Parent.append(child)
+// div.append(div);
+// document.body.append(div);
+// console.log(div);
